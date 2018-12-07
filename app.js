e96d62898a0041dc42de8d3535e3a6415c338016
Mariusz Pietrzak <pietrzakmariusz.jw@gmail.com>
2018-12-07 23:27:30 +0100

    beautify

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-(function() {
-const boxes = document.querySelectorAll('[class*="js-productsBox"]'); 
-const links = document.querySelectorAll('.js-productsLink');
+(function () {
+    const boxes = document.querySelectorAll('[class*="js-productsBox"]');
+    const links = document.querySelectorAll('.js-productsLink');
 
-function currentActiveLink() {
-    for (var item = 0; item < links.length; item++) 
-        if (links[item].classList.contains('active'))
-            return item;
-}
-
-function clickLink() {
-    console.log(boxes);
-    let previousPosition = currentActiveLink();
-
-    if (!this.classList.contains('active')) {
-        links[previousPosition].classList.remove('active');
-        this.classList.add('active');
-
-        setTimeout(() => {
-            boxes[previousPosition].classList.replace("js-productsBox--hide", "js-productsBox--disable");
-
-            boxes[currentActiveLink()].classList.replace("js-productsBox--disable", "js-productsBox--show");
-        }, 500);
-
-        boxes[previousPosition].classList.replace("js-productsBox--show", "js-productsBox--hide");
+    function currentActiveLink() {
+        for (var item = 0; item < links.length; item++)
+            if (links[item].classList.contains('active'))
+                return item;
     }
-}
 
-for (var item = 0; item < links.length; item++) {
-    links[item].addEventListener('click', clickLink, false);
-}
+    function clickLink() {
+        console.log(boxes);
+        
+        if (!this.classList.contains('active')) {
+            let previousPosition = currentActiveLink();
+            
+            links[previousPosition].classList.remove('active');
+            this.classList.add('active');
+
+            setTimeout(() => {
+                boxes[previousPosition].classList.replace("js-productsBox--hide", "js-productsBox--disable");
+
+                boxes[currentActiveLink()].classList.replace("js-productsBox--disable", "js-productsBox--show");
+            }, 500);
+
+            boxes[previousPosition].classList.add("js-productsBox--hide");
+            // boxes[previousPosition].classList.replace("js-productsBox--show", "js-productsBox--hide");
+        }
+    }
+
+    for (var item = 0; item < links.length; item++) {
+        links[item].addEventListener('click', clickLink, false);
+    }
 
 })();
