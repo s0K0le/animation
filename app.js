9724c7fa6667b72e4b89f28305379e975d12887d
Mariusz Pietrzak <pietrzakmariusz.jw@gmail.com>
2018-12-07 19:32:46 +0100

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+(function() {
+const boxes = document.querySelectorAll('[class*="js-productsBox"]'); 
+const links = document.querySelectorAll('.js-productsLink');
+
+function currentActiveLink() {
+    for (var item = 0; item < links.length; item++) 
+        if (links[item].classList.contains('active'))
+            return item;
+}
+
+function clickLink() {
+    console.log(boxes);
+    let previousPosition = currentActiveLink();
+
+    if (!this.classList.contains('active')) {
+        links[previousPosition].classList.remove('active');
+        this.classList.add('active');
+
+        setTimeout(() => {
+            boxes[previousPosition].classList.replace("js-productsBox--hide", "js-productsBox--disable");
+
+            boxes[currentActiveLink()].classList.replace("js-productsBox--disable", "js-productsBox--show");
+        }, 500);
+
+        boxes[previousPosition].classList.replace("js-productsBox--show", "js-productsBox--hide");
+    }
+}
+
+for (var item = 0; item < links.length; item++) {
+    links[item].addEventListener('click', clickLink, false);
+}
+
+})();
